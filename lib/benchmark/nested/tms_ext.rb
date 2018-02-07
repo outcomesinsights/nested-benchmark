1f310238e1bb4dbac6de0d681340ba1248e76caf
Ryan Duryea <aguynamedryan@gmail.com>
2018-02-07 13:28:38 -0800

    Add support for remainder, other fixes

--- lib/benchmark/nested/tms_ext.rb
+++ lib/benchmark/nested/tms_ext.rb
@@ -1,32 +1,40 @@
 require 'benchmark'
 
 module Benchmark
-  
   class Tms
-    
     include Enumerable
-    
+
     attr_accessor :label
     attr_writer :children
+
     def children
       @children ||= []
     end
-    
+
     def each(&block)
       children.each(&block)
     end
-    
-    def to_s(indent=0)
+
+    def to_s(opts = {})
+      opts = { indent: 0 }.merge(opts)
       lines = []
-      lines << format(FMTSTR).rstrip
+      lines << format(FORMAT).rstrip
+
+      indent = opts[:indent]
       indenter = ('>' * indent) + ' ' if indent > 0
       lines.last << " #{indenter}#{@label}"
+
       children.each do |child|
-        lines << child.to_s(indent + 1)
+        lines << child.to_s(opts.merge(indent: indent + 1))
       end
+
+      if opts[:include_remainder] && !children.empty?
+        remainder_tms = (self - children.inject(:+))
+        remainder_tms = "Remainder"
+        lines << remainder_tms.to_s(indent: indent)
+      end
+
       lines.join("\n")
     end
-    
   end
-  
 end
--- nested-benchmark.gemspec
+++ nested-benchmark.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Ryan Duryea"]
   spec.email         = ["aguynamedryan@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Add support for simple named benchmarking blocks, blocks to ignore during benchmarking, and output of benchmarks}
+  spec.description   = %q{Add support for simple named benchmarking blocks, blocks to ignore during benchmarking, and output of benchmarks}
+  spec.homepage      = "https://rubygems.org/gems/nested-benchmark/"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
